4af6b265f9da2808561aa68ba830715f1aec7129
Rene <reneruck@googlemail.com>
2012-09-16 17:21:56 +0100

    add playlist fragment

---
+++ src/de/reneruck/inear2/PlaylistFragment.java
@@ -0,0 +1,68 @@
+package de.reneruck.inear2;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+public class PlaylistFragment extends Fragment implements PropertyChangeListener{
+	
+	private AppContext appContext;
+	private List<String> currentPlaylist;
+	private ListView playlistView;
+	private PlaylistAdapter listAdapter;
+	
+	
+	public PlaylistFragment() {
+		super();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.appContext = (AppContext) getActivity().getApplicationContext();
+		this.appContext.getCurrentAudiobookBean().addPropertyChangeListener(this);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View inflated = inflater.inflate(R.layout.fragment_playlist, container);
+		this.playlistView = (ListView) inflated.findViewById(R.id.playlist);
+		setupListView();
+		return inflated;
+	}
+
+	private void setupListView() {
+		this.currentPlaylist = this.appContext.getCurrentAudiobookBean().getPlaylist();
+		this.listAdapter = new PlaylistAdapter(this.appContext, android.R.layout.simple_list_item_1, currentPlaylist);
+		this.playlistView.setAdapter(this.listAdapter);
+		this.playlistView.setOnItemClickListener(this.onPlaylistItemListener);
+	}
+	
+	private OnItemClickListener onPlaylistItemListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+			appContext.getCurrentAudiobookBean().setCurrentTrack(pos);
+		}
+	};
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		if("track".equals(event.getPropertyName()))
+		{
+			if(this.playlistView != null)
+			{
+				this.listAdapter.notifyDataSetChanged();
+			}
+		}
+	}
+}
