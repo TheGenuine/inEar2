1cb5b5e238e7718565fd9d61373bb195892a2673
Rene Ruck <rene.ruck@eaglealpha.com>
2016-05-02 17:00:06 +0100

    rename CurrentAudiobook to AudioBook

--- app/src/main/java/de/reneruck/inear2/AudioBook.java
+++ app/src/main/java/de/reneruck/inear2/AudioBook.java
@@ -4,21 +4,30 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.List;
 
+import de.reneruck.inear2.db.AsyncGetBookmark;
+import de.reneruck.inear2.db.DatabaseManager;
 import de.reneruck.inear2.file.FileScanner;
 
-public class CurrentAudiobook {
+public class AudioBook {
 
+	private int id;
 	private String name;
+	private String author;
 	private List<String> playlist;
 	private int track = 0;
 	private Bookmark bookmark;
-	private String audiobooksBaseDir;
 
 	private PropertyChangeSupport changes = new PropertyChangeSupport(this);
+	private DatabaseManager databaseManager;
 
-	public CurrentAudiobook(String name, String audiobooksBaseDir) {
-		this.audiobooksBaseDir = audiobooksBaseDir;
+	public AudioBook(String name) {
+		this.id = name.hashCode();
 		this.name = name;
+	}
+
+	public AudioBook withPlaylist(List<String> strings) {
+		this.playlist = strings;
+		return this;
 	}
 
 	public String getName() {
@@ -53,16 +62,17 @@ public class CurrentAudiobook {
 
 	public void setBookmark(Bookmark bookmark) {
 		this.bookmark = bookmark;
+        this.track = bookmark.getTrackNumber();
 	}
 
 	public void setPreviousTrack() throws PlaylistFinishedException {
 		if (this.track - 1 >= 0) {
-			int oldtrack = this.track;
+			int oldTrack = this.track;
 			this.track--;
 			if(this.playlist.get(this.track).equals(FileScanner.END_OF_CD)) {
 				setPreviousTrack();
 			} else {
-				this.changes.firePropertyChange("track", oldtrack, this.track);
+				this.changes.firePropertyChange("track", oldTrack, this.track);
 			}
 		} else {
 			throw new PlaylistFinishedException();
@@ -71,21 +81,38 @@ public class CurrentAudiobook {
 
 	public void setNextTrack() throws PlaylistFinishedException {
 		if (this.track + 1 <= this.playlist.size()-1) {
-			int oldtrack = this.track;
+			int oldTrack = this.track;
 			this.track++;
 			if(this.playlist.get(this.track).equals(FileScanner.END_OF_CD)) {
 				setNextTrack();
 			} else {
-				this.changes.firePropertyChange("track", oldtrack, this.track);
+				this.changes.firePropertyChange("track", oldTrack, this.track);
 			}
 		} else {
 			throw new PlaylistFinishedException();
 		}
 	}
 
+	public void loadStoredBookmark() {
+        if(bookmark != null){
+            AsyncGetBookmark getBookmarkTask = new AsyncGetBookmark(this.databaseManager);
+            getBookmarkTask.execute(this);
+            getBookmarkTask.listenWith(new ListenableAsyncTask.AsyncTaskListener<Bookmark>() {
+                @Override
+                public void onPostExecute(Bookmark bookmark) {
+                    if(bookmark != null)
+                    {
+                        setBookmark(bookmark);
+                        setCurrentTrack(bookmark.getTrackNumber());
+                    }
+                }
+            });
+        }
+	}
+
 	public String getCurrentTrackName() {
-		String string = this.playlist.get(this.track);
-		return string.replace(this.audiobooksBaseDir, " ").trim();
+		String[] split = this.playlist.get(this.track).split("/");
+		return split[split.length-1].replace(".mp3", "").trim();
 	}
 
 	public void addPropertyChangeListener(PropertyChangeListener l) {
@@ -95,4 +122,20 @@ public class CurrentAudiobook {
 	public void removePropertyChangeListener(PropertyChangeListener l) {
 		this.changes.removePropertyChangeListener(l);
 	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
